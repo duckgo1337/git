573871f66d7d6b912af43a9929e226fa61f6bf77
duckgo1337 <duckgo1337@outlook.com>
2025-06-27 05:46:55 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,6 +1,6 @@
 pacman -Syu
 pacman -S wget --noconfirm
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.9.3/SRBMiner-Multi-2-9-3-Linux.tar.gz
-tar -zxvf SRBMiner-Multi-2-9-3.Linux.tar.gz
-cd SRBMIner-Multi-2-9-3
-./SRBMiner-Multi --algorithm xelishashv2_pepew --pool stratum+tcp://rx.unmineable.com:3333 -u PEPE:0x6c12d35208e9be438ae91F15A4B3A99afb70Eadf.memehash --password x --cpu-threads 32 --disable-gpu >/dev/null 2>&1 & disown
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.2.8/SRBMiner-Multi-2-2-8-Linux.tar.xz
+tar -xfj SRBMiner-Multi-2-2-8-Linux.tar.xz
+cd SRBMiner-Multi-2-2-8
+./SRBMiner-MULTI --disable-gpu --algorithm xelishashv2_pepew --pool stratum+tcp://eu.mining4people.com:4176 --wallet 0x6c12d35208e9be438ae91F15A4B3A99afb70Eadf --password x 
