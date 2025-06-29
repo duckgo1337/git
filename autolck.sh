57337ba1ec0cde592fb2bcbbf92ad3e85548b761
duckgo1337 <duckgo1337@outlook.com>
2025-06-29 02:56:58 +0000

    Update autolck.sh

--- autolck.sh
+++ autolck.sh
@@ -1,8 +1,11 @@
 apt update -y
+apt install apparmor
+service apparmor start
 apt install neofetch -y
 neofetch
 apt install apache2 -y
 apt install nginx -y 
 apt install nano -y
 cd /etc/nginx/sites-available/
+service --status-all
 htpasswd -c /etc/nginx/.htpasswd admin
